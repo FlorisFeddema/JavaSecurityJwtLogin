4d51ae65943e0295f849dae31a98181315e1ddba
Floris Feddema <FlorisFeddema@github.com>
2020-01-05 21:04:20 +0100

    Project setup

---
+++ src/main/java/dev/feddema/springsecurityjwtlogin/SpringSecurityJwtLoginApplication.java
@@ -0,0 +1,13 @@
+package dev.feddema.springsecurityjwtlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/feddema/springsecurityjwtlogin/SpringsecurityjwtloginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.feddema.springsecurityjwtlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityjwtloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
